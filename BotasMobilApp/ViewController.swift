d5c829c53b259779c050997333b354ef6aaa305c
Admin <admin@admins-macbook-air.local>
2019-04-09 16:04:49 +0300

    Initial Commit

---
+++ BotasMobilApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BotasMobilApp
+//
+//  Created by Admin on 9.04.2019.
+//  Copyright © 2019 tahasvc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
